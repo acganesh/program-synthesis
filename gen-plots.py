a685f0750c1fb7af3221c29e7fdac32920ecfa57
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-12 04:11:17 +0000

    update

--- gen-plots.py
+++ gen-plots.py
@@ -5,8 +5,18 @@ def learning_curve(n):
     fname = "out/losses_vae%i.pt.pkl" % n
     with open(fname, "rb") as f:
         losses = pickle.load(f)
+    losses = list(map(float, losses))
+
+    plt.plot(losses)
+    plt.xlabel("Epoch number")
+    plt.ylabel("VAE loss")
+    plt.title("VAE learning curve - %i line programs" % n)
 
     plt.plot(losses)
 
+    plt.savefig("plots/LC%i.png" % n)
+    plt.clf()
 
-learning_curve(2)
+
+for k in range(1, 6):
+    learning_curve(k)
